59637df84606eeecc0e920a9f7a0f6b6cad9c27d
Dan <daniel@ac.com.au>
2021-09-07 23:29:30 +1000

    Realised was updating pos in wrong function.

--- SDML.js
+++ SDML.js
@@ -204,7 +204,7 @@ var Module = typeof Module !== 'undefined' ? Module : {};
     }
   
    }
-   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}], "remote_package_size": 5979, "package_uuid": "ea3f3a19-3c62-4ed5-a8d1-580c852aa053"});
+   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}], "remote_package_size": 5979, "package_uuid": "7468169b-8dee-4913-9bba-7e392f5ef69c"});
   
   })();
   
