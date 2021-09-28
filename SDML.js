b292b9d6a92f2a59de404d636c64bb142c86e886
Dan <daniel@ac.com.au>
2021-09-28 15:05:06 +1000

    Not directly using the message for tokenisation.

--- SDML.js
+++ SDML.js
@@ -204,7 +204,7 @@ var Module = typeof Module !== 'undefined' ? Module : {};
     }
   
    }
-   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "ad6605ea-3218-4ef5-aefc-996f57624816"});
+   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "0b047945-1859-4e24-a135-cf183a2c494a"});
   
   })();
   
