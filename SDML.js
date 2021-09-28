ed8212786f7b48f1dc18fc702b7afc1fb3b9ebe2
Dan <daniel@ac.com.au>
2021-09-28 11:12:13 +1000

    Set the test circle to be same size as original player.

--- SDML.js
+++ SDML.js
@@ -204,7 +204,7 @@ var Module = typeof Module !== 'undefined' ? Module : {};
     }
   
    }
-   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "0b05d498-0271-4985-8827-e83ffe6d11ba"});
+   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "0757df65-575e-441d-b417-ef509dfe6e2c"});
   
   })();
   
