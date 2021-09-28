dd932d89bca851b03ed6741c4cd6091e48802fe5
Dan <daniel@ac.com.au>
2021-09-28 22:16:44 +1000

    Clientside context now stores velocities as integers.

--- SDML.js
+++ SDML.js
@@ -204,7 +204,7 @@ var Module = typeof Module !== 'undefined' ? Module : {};
     }
   
    }
-   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "3ef770f3-0a88-4d42-94f7-534fa3fdabeb"});
+   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "ec303d7d-3ffa-4a8d-9c8b-0fc51105b87f"});
   
   })();
   
