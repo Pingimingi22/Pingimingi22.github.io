0be3161fab078d7410e474b634e76cd5377f9c1b
Dan <daniel@ac.com.au>
2021-09-28 15:27:22 +1000

    Fixed individual token debugging.

--- SDML.js
+++ SDML.js
@@ -204,7 +204,7 @@ var Module = typeof Module !== 'undefined' ? Module : {};
     }
   
    }
-   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "611f63a8-6460-4224-95c8-f4da6819f9e8"});
+   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "3efead69-e7a6-45f4-b3ac-908c47f5d73a"});
   
   })();
   
