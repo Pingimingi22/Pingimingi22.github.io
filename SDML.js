70a6f0609a6463f339dc0e3dda7583c744b5c92e
Dan <daniel@ac.com.au>
2021-09-28 10:28:50 +1000

    Added a new line character after debug message.

--- SDML.js
+++ SDML.js
@@ -204,7 +204,7 @@ var Module = typeof Module !== 'undefined' ? Module : {};
     }
   
    }
-   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "debd6339-ce1e-44d1-8059-a53d4edec6c0"});
+   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "789ed8cc-b7b1-401b-930f-0ee04b78b9dc"});
   
   })();
   
