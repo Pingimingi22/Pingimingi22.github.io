1494047980860df15830e2cbf257209a90db8cd3
Dan <daniel@ac.com.au>
2021-09-28 13:16:47 +1000

    Fixed PLAYERMOVE missing syntax bug.

--- SDML.js
+++ SDML.js
@@ -204,7 +204,7 @@ var Module = typeof Module !== 'undefined' ? Module : {};
     }
   
    }
-   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "cc14934d-527d-4914-8af3-f1d91cbcc104"});
+   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "dcce3426-4bdd-4d33-a6b9-6147dfb0bc24"});
   
   })();
   
