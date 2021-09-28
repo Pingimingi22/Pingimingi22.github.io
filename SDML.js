05e81487ae2840842e460c5ba73273df704152b1
Dan <daniel@ac.com.au>
2021-09-28 15:01:48 +1000

    Modified GetToken() again.

--- SDML.js
+++ SDML.js
@@ -204,7 +204,7 @@ var Module = typeof Module !== 'undefined' ? Module : {};
     }
   
    }
-   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "9681d2c3-eac2-4fe1-ab45-1bdac3be79b9"});
+   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "ad6605ea-3218-4ef5-aefc-996f57624816"});
   
   })();
   
