a1e7c3ac6209bc78c94763c7a30b837ea78d9d8f
Dan <daniel@ac.com.au>
2021-09-28 15:43:37 +1000

    Made string variables for tokens.

--- SDML.js
+++ SDML.js
@@ -204,7 +204,7 @@ var Module = typeof Module !== 'undefined' ? Module : {};
     }
   
    }
-   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "7ce22158-62a3-413b-898a-ef8f82498608"});
+   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "8770e5d9-780b-4492-b221-53a562056208"});
   
   })();
   
