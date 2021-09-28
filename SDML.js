1cdbe730f6ff072c73dc0ed3277f6d89997fd634
Dan <daniel@ac.com.au>
2021-09-28 13:15:08 +1000

    Client PLAYERMOVE message now transmits ID.

--- SDML.js
+++ SDML.js
@@ -204,7 +204,7 @@ var Module = typeof Module !== 'undefined' ? Module : {};
     }
   
    }
-   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "5e1a8301-98f3-47f7-b680-6224e7f6a409"});
+   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "cc14934d-527d-4914-8af3-f1d91cbcc104"});
   
   })();
   
