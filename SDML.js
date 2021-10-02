4f9bdd76a936f1c8c90f99946fdb9fe5230be904
Dan <daniel@ac.com.au>
2021-10-02 15:53:30 +1000

    Client spawn packet identifier changed to 34 for testing.

--- SDML.js
+++ SDML.js
@@ -204,7 +204,7 @@ var Module = typeof Module !== 'undefined' ? Module : {};
     }
   
    }
-   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "167258c2-31c6-4172-b027-59c8e85d1d2d"});
+   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "f7f08d57-bab2-47ae-a562-b954a9283708"});
   
   })();
   
