0cd312de27becb0fd4560370624ad30705899814
Dan <daniel@ac.com.au>
2021-10-02 20:02:15 +1000

    Client now loops based on string_view length().

--- SDML.js
+++ SDML.js
@@ -204,7 +204,7 @@ var Module = typeof Module !== 'undefined' ? Module : {};
     }
   
    }
-   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "22aa1d5c-5c28-47d6-8942-b55e329d3dac"});
+   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "823229c1-02d4-4521-b581-7fb5052e6918"});
   
   })();
   
