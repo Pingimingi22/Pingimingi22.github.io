333d441e3b8afbe8541cfb07c7ab841a34d229a4
Dan <daniel@ac.com.au>
2021-10-02 17:00:28 +1000

    Client directly reads from websocketEvent.data now.

--- SDML.js
+++ SDML.js
@@ -204,7 +204,7 @@ var Module = typeof Module !== 'undefined' ? Module : {};
     }
   
    }
-   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "f00f080b-cdb9-45c3-b574-cf32e05af4bd"});
+   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "6c4db072-b04d-493e-a8f2-a24294bb7114"});
   
   })();
   
