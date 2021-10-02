e0d4cea9a90a4cfb307132f3811f02f6ddcd4d1f
Dan <daniel@ac.com.au>
2021-10-02 16:54:43 +1000

    Client no longer reserves 100 slots for std::vector.

--- SDML.js
+++ SDML.js
@@ -204,7 +204,7 @@ var Module = typeof Module !== 'undefined' ? Module : {};
     }
   
    }
-   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "c7898b97-7286-4a9c-8f16-9959c78716f0"});
+   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "f00f080b-cdb9-45c3-b574-cf32e05af4bd"});
   
   })();
   
