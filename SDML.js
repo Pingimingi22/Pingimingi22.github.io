fc2904ce55d457c57d85e9089203753c50060c00
Dan <daniel@ac.com.au>
2021-10-04 21:47:05 +1100

    Casting player's velocities to floats when updating movement.

--- SDML.js
+++ SDML.js
@@ -204,7 +204,7 @@ var Module = typeof Module !== 'undefined' ? Module : {};
     }
   
    }
-   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "d2c03b07-c8e7-49de-b973-490cc5a2c4f3"});
+   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "e492bc44-dfe3-4bc2-94a3-64827a9fe9d2"});
   
   })();
   
