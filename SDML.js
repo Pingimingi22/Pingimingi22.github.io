3195351c48a6a4fb258cfb6adb04488a6c07d276
Dan <daniel@ac.com.au>
2021-10-04 18:15:08 +1100

    Fixed client position fix bug.

--- SDML.js
+++ SDML.js
@@ -204,7 +204,7 @@ var Module = typeof Module !== 'undefined' ? Module : {};
     }
   
    }
-   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "d3fa7c17-1a0f-47d4-97b2-5d7da75e245b"});
+   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "a93e9320-e572-4425-98b8-44b236dab03e"});
   
   })();
   
