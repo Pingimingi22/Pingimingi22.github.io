9ea6c1cb58aa768db4491ec4e6523b3aa87af20d
Dan <daniel@ac.com.au>
2021-10-05 12:20:29 +1100

    Fixed issue where client spawns other players at an incorrect position.

--- SDML.js
+++ SDML.js
@@ -204,7 +204,7 @@ var Module = typeof Module !== 'undefined' ? Module : {};
     }
   
    }
-   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "0913c05f-affd-48f5-8015-df1e3eaf89d3"});
+   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "3ec8390e-b95a-4a97-972e-69904689a963"});
   
   })();
   
