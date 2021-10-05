3efb45eee870ab9aa64c694283c6d4f353998338
Dan <daniel@ac.com.au>
2021-10-05 12:12:03 +1100

    Client's other players now now update their position with floats.

--- SDML.js
+++ SDML.js
@@ -204,7 +204,7 @@ var Module = typeof Module !== 'undefined' ? Module : {};
     }
   
    }
-   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "587a0543-237b-47f3-8f55-cf29f4de4108"});
+   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "12d9ecc6-bfda-4fd1-8728-4882626671b3"});
   
   })();
   
