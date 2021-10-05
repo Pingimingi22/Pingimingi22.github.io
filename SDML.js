86cc1f63ebc56f66fcc0e35e4353c869433f3b85
Dan <daniel@ac.com.au>
2021-10-05 12:17:00 +1100

    Client now updates other clients with delta time.

--- SDML.js
+++ SDML.js
@@ -204,7 +204,7 @@ var Module = typeof Module !== 'undefined' ? Module : {};
     }
   
    }
-   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "12d9ecc6-bfda-4fd1-8728-4882626671b3"});
+   loadPackage({"files": [{"filename": "/test_images/hello_world.png", "start": 0, "end": 5979, "audio": 0}, {"filename": "/test_images/ship.png", "start": 5979, "end": 6813, "audio": 0}], "remote_package_size": 6813, "package_uuid": "0913c05f-affd-48f5-8015-df1e3eaf89d3"});
   
   })();
   
